db85b3466968b0b1ddbe1d3b369ba664038cf544
lautaroolmedo <lolmedo@arquant.com.ar>
2026-01-29 21:23:46 -0300

    added ci-cd.yaml

--- internal/service/order/create_order.go
+++ internal/service/order/create_order.go
@@ -7,36 +7,6 @@ import (
 	"time"
 )
 
-/*func (s *Service) CreateOrder(ctx context.Context, productID string, quantity int) error {
-	return s.TransactionManager.WithTransaction(ctx, func(txCtx context.Context) error {
-
-		product, err := s.ProductService.GetProductByID(ctx, productID)
-		if err != nil {
-			return err
-		}
-
-		if product.Stock < quantity {
-			return domain.ErrInsufficientStock
-		}
-
-		newStock := product.Stock - quantity
-
-		if err := s.ProductService.UpdateProductStock(txCtx, productID, newStock); err != nil {
-			return err
-		}
-
-		order := &domain.Order{
-			ID:        uuid.New().String(),
-			ProductID: productID,
-			Quantity:  quantity,
-			Total:     product.Price * float64(quantity),
-			Date:      time.Now(),
-		}
-
-		return s.StorageRepository.CreateOrder(txCtx, *order)
-	})
-}*/
-
 func (s *Service) CreateOrder(
 	ctx context.Context,
 	productID string,
